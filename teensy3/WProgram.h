d756c22cb513f48be2b595b85887572f1df8e837
Ellis Rahhal <ellis.rahhal@gmail.com>
2022-01-22 22:49:42 -0800

    commented out ambiguated random

--- teensy3/WProgram.h
+++ teensy3/WProgram.h
@@ -84,7 +84,7 @@ void noTone(uint8_t pin);
 
 // WMath prototypes
 int32_t random(void);
-uint32_t random(uint32_t howbig);
+// uint32_t random(uint32_t howbig);
 int32_t random(int32_t howsmall, int32_t howbig);
 void randomSeed(uint32_t newseed);
 void srandom(unsigned int newseed);
